cf451ecf8a686e90272129a881c79fb6483f0a06
aravindiiitb <konakondla.sak@iiitb.org>
2016-03-27 03:59:24 +0530

    added login and signup screens with working signup functionality

---
+++ app/src/main/java/com/example/perseus14/vehicletracking/Config.java
@@ -0,0 +1,12 @@
+package com.example.perseus14.vehicletracking;
+
+/**
+ * Created by dell pc on 27/03/2016.
+ */
+public class Config {
+
+    public Config(){
+    }
+
+    public static String URL_CREATE_USER = "http://10.0.0.1/VehicleTracking/create_user.php";
+}
--- app/src/main/java/com/example/perseus14/vehicletracking/MainActivity.java
+++ app/src/main/java/com/example/perseus14/vehicletracking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.perseus14.vehicletracking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        btnLogin = (Button) findViewById(R.id.login);
+        btnSignup = (Button) findViewById(R.id.signup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -46,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
